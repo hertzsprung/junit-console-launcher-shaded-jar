a5d43ae2c45bf218aac0f65cc56d80311f908219
James Shaw <james.shaw@thefloow.com>
2022-01-12 09:47:43 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Minimal example of JUnit 5 console launcher failing to run `ExampleTest.test` within a shaded JAR.
+
+See https://stackoverflow.com/q/70666391/150884.
---
+++ src/test/java/example/ExampleTest.java
@@ -0,0 +1,10 @@
+package example;
+
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+    @Test
+    void test() {
+        System.out.println("ExampleTest.test");
+    }
+}
